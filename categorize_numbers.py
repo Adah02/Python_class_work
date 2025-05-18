6b544fe8ac45b02ef94861cfa83a80b7fb3b3732
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-05-18 22:34:26 +0100

    categorizing divisible numbers

---
+++ categorize_numbers.py
@@ -0,0 +1,23 @@
+def categorize_numbers(values, divisor):
+  divisible = 0
+  non_divisible = 0
+  if values // divisor == 0:
+    print(values, end =" ")
+    divisible += 1
+  if values // divisor != 0:
+    non_divisible += 1
+
+  if divisible >= 1:
+    return values
+  else: print('No divisible number found')
+  return non_divisible
+    
+
+
+
+divisor = float(input('Enter divisor: '))
+values = input('Enter number: ')
+while values != 0:
+  values = float(input('Enter number: '))
+result = categorize_numbers(values,divisor)
+print(result)
