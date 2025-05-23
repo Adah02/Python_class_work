bc4717557c46397493c5b18df64cfe3f6a33acfe
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-05-23 17:02:00 +0100

    Practicals on list

--- Array_of_student_scores.py
+++ Array_of_student_scores.py
@@ -21,3 +21,24 @@ for item in cart:
 #Printing multiple items from  in the same line.
 
 print(cart[1:55])
+
+for value in enumerate(cart):
+  print(value)
+
+
+scores.append(67)
+scores.pop(3)
+cart[4].insert(0, 8)
+scores.extend([34,67,87,65])
+
+#(+ and *) are regarded as overloaded operators.
+
+print(scores)
+print(cart[0].upper())
+
+
+print(scores[0:6:2])
+print(scores[::2])
+
+new_list = cart + scores
+print(new_list)
